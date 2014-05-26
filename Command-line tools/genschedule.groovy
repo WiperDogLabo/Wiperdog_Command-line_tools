43361d0fc7d03a0bdebad34e601d8338d8b5a85c
nguyenxuanluong-luvina <luongnx512@gmail.com>
2014-05-26 11:01:57 +0700

    Update genschedule.groovy

--- Command-line tools/genschedule.groovy
+++ Command-line tools/genschedule.groovy
@@ -1,56 +1,51 @@
-public class GenSchedule{
-	public static void main(String [] args){
-		try {
-			def trgFileName ;
-			def jobName;
-			def schedule;		
-			for(int i=0 ; i< args.length; i++ ){
-				//get trigger file name
-				if(args[i].equalsIgnoreCase("-f")){
-					if(!args[i+1].trim().equals("") ){
-						trgFileName = args[i+1]
-					} else {
-						println "Missing file name value"
-						return
-					}
-				}
-				//get job name
-				if(args[i].equalsIgnoreCase("-j")){
-					if(!args[i+1].trim().equals("") ){
-						jobName = args[i+1]
-					} else {
-						println "Missing job name value"
-						return
-					}
-				}
-				
-				//get Schedule name
-				if(args[i].equalsIgnoreCase("-s")){
-					if(!args[i+1].trim().equals("") ){
-						schedule = args[i+1]
-					} else {
-						println "Missing schedule value"
-						return
-					}
-				}
+	def trgFileName ;
+	def jobName;
+	def schedule;		
+	for(int i=0 ; i< args.length; i++ ){
+		//get trigger file name
+		if(args[i].equalsIgnoreCase("-f")){
+			if(!args[i+1].trim().equals("") ){
+				trgFileName = args[i+1]
 			}
-			if(trgFileName == null){
-				trgFileName = jobName + ".trg"
+		}
+		//get job name
+		if(args[i].equalsIgnoreCase("-j")){
+			if(!args[i+1].trim().equals("") ){
+				jobName = args[i+1]
 			}
-			def basedir = new File(getClass().protectionDomain.codeSource.location.path).parent
-			File trgFile = new File(basedir + "/../var/job/",trgFileName);
-			if(!trgFile.exists()){
-				trgFile.createNewFile()
-			} 
-			def trgTxt = trgFile.getText()
-			if(!trgTxt.trim().equals("")){
-				trgTxt+= "\n"
+		}
+		
+		//get Schedule name
+		if(args[i].equalsIgnoreCase("-s")){
+			if(!args[i+1].trim().equals("") ){
+				schedule = args[i+1]
 			}
-			trgTxt+= "job: \"${jobName}\" , schedule : \"${schedule}\""			
-			trgFile.setText(trgTxt)
-			println "Finished! Trigger file create at : ${trgFile.getCanonicalPath()}"
-		}catch(Exception ex){
-			ex.printStackTrace()
 		}
 	}
-}
+
+	if(jobName == null) {
+		println "Missing job name !"
+		return
+	}
+
+	if(schedule == null) {
+		println "Missing schedule !"
+		return
+	}
+	if(trgFileName == null){
+		trgFileName = jobName + ".trg"
+	}
+	println getClass().protectionDomain.codeSource
+	def basedir = new File(getClass().protectionDomain.codeSource.location.path).parent
+	File trgFile = new File(basedir + "/../var/job/",trgFileName);
+	if(!trgFile.exists()){
+		trgFile.createNewFile()
+	} 
+	def trgTxt = trgFile.getText()
+	if(!trgTxt.trim().equals("")){
+		trgTxt+= "\n"
+	}
+	trgTxt+= "job: \"${jobName}\" , schedule : \"${schedule}\""			
+	trgFile.setText(trgTxt)
+	println "Finished! Trigger file create at : ${trgFile.getCanonicalPath()}"
+
